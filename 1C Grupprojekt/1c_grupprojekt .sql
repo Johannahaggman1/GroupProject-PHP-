01f9c790df014a8f900359fc63736d8fa74eae2c
cbergelin <carl.bergelin@mediainstitutet.se>
2020-02-24 13:22:07 +0100

    mappstruktur

--- 1C Grupprojekt/1c_grupprojekt .sql
+++ 1C Grupprojekt/1c_grupprojekt .sql
@@ -62,7 +62,8 @@ CREATE TABLE `users` (
   `id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `email` varchar(50) NOT NULL,
-  `password` varchar(50) NOT NULL
+  `password` varchar(50) NOT NULL,
+  `role` varchar(6) NOT NULL,
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
