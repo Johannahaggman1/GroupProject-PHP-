e43204a095993ce3452aa3e167c8eb28dfc72620
Andreas Wallström <user.name>
2020-03-04 11:06:53 +0100

    edit commit 2.5

--- 1C Grupprojekt/Includes/delete_post.php
+++ 1C Grupprojekt/Includes/delete_post.php
@@ -4,9 +4,12 @@ include("show_posts.php");
 
 $post_id = $_GET['post'];
 
-$query = "DELETE from posts where id =:id";
+//$query_post = "DELETE * FROM posts WHERE id =:id";
+//$query_comment = "DELETE * FROM comments WHERE "
+$query = "DELETE FROM comments where postID = :post_id; DELETE FROM posts WHERE id = :post_id";
+
 $sth = $dbh->prepare($query);
-$sth->bindParam(':id', $post_id);
+$sth->bindParam(':post_id', $post_id);
 $return = $sth->execute();
 
 if (!$return) {
