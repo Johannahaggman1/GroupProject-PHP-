7b02a7d79b2a12328e1465d2df50ce79103e1682
cbergelin <carl.bergelin@mediainstitutet.se>
2020-03-12 14:00:15 +0100

    done!

--- 1C Grupprojekt/Views/home.php
+++ 1C Grupprojekt/Views/home.php
@@ -7,44 +7,55 @@
     <title>Document</title>
 </head>
 <body>
-<div class="body_wrapper">
-<?php
-session_start();
-echo (isset($_SESSION['username']) ? "<div class='login-user-text'> Inloggad som: " . $_SESSION['username'] . "</div>" : '');
-if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'){
-    echo "<div class='header_wrapper'>";
-    echo "<div class='login-logout_wrapper'>";
-    //echo "<p>Du är admin!!</p>";
-    echo "<div class='header-link-1'>";
-    echo "<button><a href='index.php?page=writepost'><i class='far fa-edit fa-2x'></i></a></button>";
+    <?php
+    session_start(); 
+    echo "<div class='login-user-text'>";
+    echo (isset($_SESSION['username']) ? " Inloggad som: " . $_SESSION['username'] : '');
+    echo (isset($_SESSION['username']) ? "<button class='logout-btn'><a href='Includes/logout_functions.php'>Logga Ut</a></button>" : "");
     echo "</div>";
-    
+    ?> 
+<div class="body_wrapper">
 
-}
-?>
+
+<?php
+
+    echo "<div class='header_wrapper'>";
     
+    
+if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'){    
+    echo "<button><a href='index.php?page=writepost'><i class='far fa-edit fa-2x'></i></a></button>";
+}
+    
+if (!isset($_SESSION['username'])){
+?>
+    <div id="loginregister_btns">
     <form method="POST" action="index.php?page=signup">
     <button class ="btn-reg-login" type="submit">Registrera</button>
     </form>
     <form method="POST" action="index.php?page=login">
     <button class ="btn-reg-login" type="submit">Logga In</button>
     </form>
-
+    </div>
 
 <?php
+}
+    ;
     echo "<div class='header-link-2'>";
-    echo (isset($_SESSION['username']) ? "<button><a href='Includes/logout_functions.php'>Logga Ut</a></button>" : "");
-    echo "</div>";
-    echo "</div>";
     
+    
+    echo "</div>";
+    ;
+    
+    echo "</div>";
+    echo "</div>";
 ?>
     <div class="header-text">
     <center><h1>BLOGGEN</h1></center>
     <h6>MILHOUSE FINA BLOGGSIDA</h6>
 
     </div>
-
-    </div>
+        
+    
 
     <div class="content_container">
     <h2>Senaste inläggen:</h2>
@@ -52,7 +63,7 @@ if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'){
     include('Views/show_posts.php');
 
     ?>
-    </div>
+    
 
     <?php
     if (isset($_GET['registered']) && $_GET['registered'] == 'true'){
--- 1C Grupprojekt/Views/show_posts.php
+++ 1C Grupprojekt/Views/show_posts.php
@@ -38,13 +38,13 @@
             echo $row['date'];
             echo "</center>";
             echo "<div class='post_footer'>";
+            if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'){
             echo "<center><a href='index.php?page=editpost&post=" . $post_id . "'>Redigera inlägg</a></center>";
             echo "</br>";
             echo "<center><a href='Includes/delete_post.php?post=" . $post_id . "'><i class='fas fa-trash-alt fa-2x'></i></a></center>";
-            echo "</div>";
-            echo "</div>";
+            }
             echo "</br>";
-
+            
             /*$postDeleted = new GBPost($dbh);
 
             $postDeleted->fetchAll($post_id);
@@ -60,11 +60,11 @@
             
             if (isset($_GET['showcomments']) && $_GET['showcomments'] == 'true'){
                 echo "<div class='comments_link'>";
-                echo "<a href='index.php?post=$post_id'>". $sth_comments_amount->rowCount() . " Kommentarer</a><hr />";
+                echo "<br/><a href='index.php?post=$post_id'>". $sth_comments_amount->rowCount() . " Kommentarer</a><hr />";
                 echo "</div>";
             }
             else{
-                echo "<a href='index.php?post=$post_id&showcomments=true'>". $sth_comments_amount->rowCount() . " Kommentarer</a><hr />";
+                echo "<br/><a href='index.php?post=$post_id&showcomments=true'>". $sth_comments_amount->rowCount() . " Kommentarer</a><hr />";
                 echo "<hr class='hr'>";
             }
             
@@ -77,24 +77,27 @@
                 $comments->fetchAll($post_id);
                 
                 foreach($comments->getPosts() as $comments){
+                echo "<center>";
                 echo "<b>Användare: </b>" .  $comments['username'] . "<br />";
-                echo $comments['content'] . "<br />";
-                echo $comments['date'] . "<br />";
+                echo $comments['content']  . "<br />";
+                echo $comments['date']  . "<br />";
                 
                 if (isset($_SESSION['role']) && $_SESSION['role'] == 'admin'){
                     echo "<a href='Includes/delete_comment.php?post=" . $post_id . "&id=" . $comments['id'] . "'>Ta Bort</a><br />";
                 }
-
+               echo "<hr />";
                 }
 
                   if (isset($_SESSION['id']) && $_SESSION['id'] == true){
                     include('Views/comment.php');
                 }  
                 else{
-                    echo "Logga in för att kommentera!";
+                    echo "Logga in för att kommentera!<hr/>";
                 }
             }
-            
+            echo "</center>";
+            echo "</div>";
+            echo "</div>";
     }
 
     
@@ -102,7 +105,7 @@
     $query_blogposts = "SELECT id, userID, title, description, category, image, date FROM posts ORDER BY date $order";
     $rows_posts = $dbh->query($query_blogposts);
     
-
+    echo "<br/ >";
     if(isset($_GET['post']) == true){
     while($row = $rows_posts->fetch(PDO::FETCH_ASSOC)){
         //echo "<center>";
